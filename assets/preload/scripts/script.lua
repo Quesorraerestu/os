127bc8adb3c050ac6267452606e02660c7a75b64
Quesorraerestu <113881206+Quesorraerestu@users.noreply.github.com>
2022-10-23 18:43:59 -0500

    Add files via upload

--- assets/preload/scripts/script.lua
+++ assets/preload/scripts/script.lua
@@ -1,2 +1,5 @@
-var sus = true
-// you can remplace this script with anything else
+function onCreate()
+makeLuaText('watermark', songName .. " | Port by fnf mods", 0, 25, 500); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark', 20);
+    addLuaText('watermark');
+end
